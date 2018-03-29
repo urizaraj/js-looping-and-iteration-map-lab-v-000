b66848bc849271aaaa9d729151f60e02cc2123d2
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 17:52:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function nameToAttributes(drivers) {
 }
 function attributesToPhrase(drivers) {
   return drivers.map((driver) => {
-    console.log('${driver.name} is from ${driver.hometown}')
     return '${driver.name} is from ${driver.hometown}'
   })
 }
